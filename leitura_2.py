fa3d085141b2fab2760c23a746514990fa91fcbe
Vitor Cruz <vitorcolatusso@gmail.com>
2026-06-11 21:06:32 -0300

    pendulo completo

--- leitura_2.py
+++ leitura_2.py
@@ -1,61 +1,55 @@
-import cv2
-import numpy as np
-import pandas as pd
+import cv2 #visao
+import numpy as np #matematica
+import pandas as pd #csv
 
-# ========= CONFIG =========
 video_path = "pendulo_preto.mp4"
 output_csv = "pendulo_posicoes.csv"
 
-# AJUSTE MANUAL (IMPORTANTE)
-# Coloque um retângulo que contenha a massa no primeiro frame
-#x0, y0, w, h = 880, 500, 120, 120  # <-- MUDE ISSO
 x0, y0 = 1100, 720
 w, h = 100, 100
-pad = 80  # área de busca ao redor do ponto
-# ==========================
+pad = 80  # area ao redor do ponto
 
 cap = cv2.VideoCapture(video_path)
 
 if not cap.isOpened():
-    raise RuntimeError("Erro ao abrir o vídeo")
+    raise RuntimeError("Erro ao abrir vídeo")
 
-fps = cap.get(cv2.CAP_PROP_FPS)
-frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+fps = cap.get(cv2.CAP_PROP_FPS) #num fps do video
+frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) #total de frames
 
 print(f"FPS: {fps}")
 print(f"Total de frames: {frame_count}")
 
-dados = []
+dados = [] #tempo, pos x, pos y
 
-# posição inicial
 cx_prev = x0 + w // 2
 cy_prev = y0 + h // 2
 
-trajetoria = []
+trajetoria = [] #pontos lidos a cada frame
 
-frame_idx = 0
+frame_idx = 0 #contado de frames
 
 while True:
     ret, frame = cap.read()
     if not ret:
-        break
+        break #termina o loop quando o video acaba
 
-    h_frame, w_frame = frame.shape[:2]
+    h_frame, w_frame = frame.shape[:2] #altura e largura do video
 
     # Região de busca
     x1 = max(cx_prev - pad, 0)
     y1 = max(cy_prev - pad, 0)
-    x2 = min(cx_prev + pad, w_frame - 1)
-    y2 = min(cy_prev + pad, h_frame - 1)
+    x2 = min(cx_prev + pad, w_frame - 1) #limita a busca nas bordas da imagem
+    y2 = min(cy_prev + pad, h_frame - 1) #limita a busca nas bordas da imagem
 
-    crop = frame[y1:y2, x1:x2]
+    crop = frame[y1:y2, x1:x2] #recorta a imagem na parte da busca
 
     if crop.size == 0:
         continue
 
     # Processamento
-    gray = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+    gray = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY) #aqui converte a imagem para a escala do cinza
+    gray = cv2.GaussianBlur(gray, (5, 5), 0) #aplica o desfoque gaussiano, diz-se que ajuda na detecção
 
     # Threshold automático
     #_, thresh = cv2.threshold(
@@ -67,18 +61,19 @@ while True:
 
     _, thresh = cv2.threshold(
     gray, 80, 255, cv2.THRESH_BINARY_INV
-    )
+    ) #Transforma a parte branca do video em escura, e o pendulo preto em branco
 
-    # Limpeza de ruído
     kernel = np.ones((3, 3), np.uint8)
     thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
     thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+    #tratamentos da imagem
 
-    # Contornos
+    # encontra os contornos dos objetos brancos
     contours, _ = cv2.findContours(
         thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
     )
 
+    #calcular as coordenadas do centro do pendulo
     if len(contours) > 0:
         c = max(contours, key=cv2.contourArea)
 
@@ -88,21 +83,21 @@ while True:
                 cx_prev = int(M["m10"] / M["m00"]) + x1
                 cy_prev = int(M["m01"] / M["m00"]) + y1
 
-    # Salva trajetória
+    # adiciona os valores na trajetória
     trajetoria.append((cx_prev, cy_prev))
 
-    # Salva dados
+    # calcula o tempo 
     t = frame_idx / fps
+    # salva toso os valores na lista de dados para enviar ao arquivo .csv
     dados.append([frame_idx, t, cx_prev, cy_prev])
 
-    # ===== DESENHOS =====
-    # ponto verde (massa)
+    # desenha o ponto verde na massa
     cv2.circle(frame, (cx_prev, cy_prev), 6, (0, 255, 0), -1)
 
-    # região de busca (azul)
+    # faz o quadrado azul na área de busca
     cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 1)
 
-    # trajetória (amarelo)
+    # desenha a trajetória amarela
     for i in range(1, len(trajetoria)):
         cv2.line(frame, trajetoria[i - 1], trajetoria[i], (0, 255, 255), 2)
 
@@ -118,8 +113,7 @@ while True:
 cap.release()
 cv2.destroyAllWindows()
 
-# Salvar CSV
-df = pd.DataFrame(dados)
+df = pd.DataFrame(dados) #transforma a lista em uma tabela p salvar no .csv
 df.columns = ["frame", "tempo_s", "x_px", "y_px"]
 df.to_csv(output_csv, index=False)
 
--- oha.py
+++ oha.py
@@ -1,28 +1,27 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from scipy.optimize import curve_fit
+from scipy.optimize import curve_fit #encontra a função da curva
 
 df = pd.read_csv("pendulo_posicoes.csv")
 
 t = df["tempo_s"].values
 x = df["x_px"].values
 
-# remove offset (centraliza)
-x = x - np.mean(x)
+x = x - np.mean(x) #centraliza os valores de oscilação, como não há uma referência no vídeo, ajuda muito
 
 def modelo(t, A, b, omega, phi):
     return A * np.exp(-b * t) * np.cos(omega * t + phi)
 
-# chutes iniciais
-A0 = np.max(x)
-b0 = 0.02
-omega0 = 4.2   # baseado no cálculo teórico
+#chutes para o curve_fit encontrar os valores da função
+A0 = np.max(x) #procura x max
+b0 = 0.02 #chuta valor baixo p coeficiente de amortecimento
+omega0 = 5.5   # baseado no cálculo teórico
 phi0 = 0
 
 p0 = [A0, b0, omega0, phi0]
 
-params, _ = curve_fit(modelo, t, x, p0=p0, maxfev=20000)
+params, _ = curve_fit(modelo, t, x, p0=p0, maxfev=20000) #testa os valores até se encaixarem
 
 A, b, omega, phi = params
 
@@ -36,7 +35,7 @@ Q = omega / (2 * b)
 print(f"Q = {Q:.2f}")
 
 # gráfico
-t_fit = np.linspace(t.min(), t.max(), 2000)
+t_fit = np.linspace(t.min(), t.max(), 3600)
 x_fit = modelo(t_fit, *params)
 
 plt.plot(t, x, ".", label="dados")
